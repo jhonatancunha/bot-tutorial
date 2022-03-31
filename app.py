5b3d61d0477aab3be81f452ab15a5855afa56742
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2022-03-30 21:01:56 -0300

    :sparkles: Adicionando mensagem de pull request aceito e deletando branch.

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ import os
 from flask import Flask, request
 from github import Github, GithubIntegration
 
+# https://smee.io/7dXBFbbaqeCdtxNl
+
 app = Flask(__name__)
 
-app_id = '<your_app_number_here>'
+app_id = 184666
 
 # Read the bot certificate
 with open(
@@ -12,24 +14,33 @@ with open(
         'r'
 ) as cert_file:
     app_key = cert_file.read()
-    
+
 # Create an GitHub integration instance
 git_integration = GithubIntegration(
     app_id,
     app_key,
 )
 
-def pr_opened_event(repo, payload):
+
+def delete_branch_after_accepted_pr(repo, branch_name):
+    branch = repo.get_git_ref("heads/%s" % branch_name)
+    branch.delete()
+
+
+def pr_accepted_event(repo, payload):
     pr = repo.get_issue(number=payload['pull_request']['number'])
     author = pr.user.login
 
-    is_first_pr = repo.get_issues(creator=author).totalCount
+    # is_first_pr = repo.get_issues(creator=author).totalCount
 
-    if is_first_pr == 1:
-        response = f"Thanks for opening this pull request, @{author}! " \
-                   f"The repository maintainers will look into it ASAP! :speech_balloon:"
-        pr.create_comment(f"{response}")
-        pr.add_to_labels("needs review")
+    # if is_first_pr == 1:
+    response = f"Seu pull request foi aceito, @{author}! " \
+        f"Obrigado por sua contribuição :smiling_face_with_three_hearts:"
+    pr.create_comment(f"{response}")
+
+    branch_name = payload['pull_request']['head']['ref']
+    delete_branch_after_accepted_pr(repo, branch_name)
+
 
 @app.route("/", methods=['POST'])
 def bot():
@@ -48,11 +59,13 @@ def bot():
     )
     repo = git_connection.get_repo(f"{owner}/{repo_name}")
 
-    # Check if the event is a GitHub pull request creation event
-    if all(k in payload.keys() for k in ['action', 'pull_request']) and payload['action'] == 'opened':
-        pr_opened_event(repo, payload)
+    # Verifica se pull request foi aceito
+    if all(k in payload.keys() for k in ['action', 'pull_request']) and payload['action'] == 'closed':
+        if payload['pull_request']['merged']:
+            pr_accepted_event(repo, payload)
 
     return "", 204
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
